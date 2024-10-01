7691ee7b7c59e2039805720574681ce86cb1b613
Awoyemi Victor A <awoyemivictora@awoyemivictora>
2024-10-01 23:26:54 +0100

    lesson 1 assignments completed

---
+++ lesson-01/1h.js
@@ -0,0 +1,6 @@
+const tshirt = 10;
+const socks = 8;
+const plates = 20;
+
+const total = tshirt + socks + plates;
+console.log(total);
---
+++ lesson-01/1i.js
@@ -0,0 +1,7 @@
+const account = 100;
+const lunch = 20;
+const dinner = 50;
+const salary = 2000;
+
+const leftOver = account - lunch - dinner + salary;
+console.log(leftOver);
